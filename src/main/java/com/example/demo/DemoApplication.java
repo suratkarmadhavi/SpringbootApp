07dc35671c2553c89a679f5b5e198d2291d82d08
suratkarmadhavi <94161396+suratkarmadhavi@users.noreply.github.com>
2023-05-07 13:44:23 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+	
+	@GetMapping("/") 
+	public String getMessage()
+	{
+		return "Openshift deployment is successful";
+	}
+	
+	@PostMapping("/{name}")
+	public String postMessage(@PathVariable String name)
+	{
+		return "Your name is "+name;
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
